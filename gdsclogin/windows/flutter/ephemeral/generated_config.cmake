938734010abe72416e2af2c24c11bb18c706801e
Pratiksha Goswami <104529563+Pratikshathisside@users.noreply.github.com>
2023-01-27 23:46:11 +0530

    Add files via upload

---
+++ gdsclogin/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\PRATIKSHA GOSWAMI\\Desktop\\flutter\\gdsclogin" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\PRATIKSHA GOSWAMI\\Desktop\\flutter\\gdsclogin"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\PRATIKSHA GOSWAMI\\Desktop\\flutter\\gdsclogin\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\PRATIKSHA GOSWAMI\\Desktop\\flutter\\gdsclogin"
+  "FLUTTER_TARGET=C:\\Users\\PRATIKSHA GOSWAMI\\Desktop\\flutter\\gdsclogin\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\PRATIKSHA GOSWAMI\\Desktop\\flutter\\gdsclogin\\.dart_tool\\package_config.json"
+)
